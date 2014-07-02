04ee82bde45804591c920e3f12535a1594942e35
Andrew Harris <andrew@noblesamurai.com>
2014-07-02 14:22:45 +1000

    fix typo

--- protractor.config.js
+++ protractor.config.js
@@ -13,7 +13,7 @@ var config = {
 if (process.env.TRAVIS_BUILD_NUMBER) {
   config.sauceUser = process.env.SAUCE_USERNAME,
   config.sauceKey = process.env.SAUCE_ACCESS_KEY,
-  config.capabilitied = {
+  config.capabilities = {
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
