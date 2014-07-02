ae408b13a8aceeb7c025526f310a57bc309af363
Andrew Harris <andrew@noblesamurai.com>
2014-07-02 18:15:36 +1000

    try sauce connect 4.3

---
+++ bin/sauce_connect_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Setup and start Sauce Connect for your TravisCI build
+# This script requires your .travis.yml to include the following two private env variables:
+# SAUCE_USERNAME
+# SAUCE_ACCESS_KEY
+# Follow the steps at https://saucelabs.com/opensource/travis to set that up.
+#
+# Curl and run this script as part of your .travis.yml before_script section:
+# before_script:
+#   - ./bin/sauce_connect_setup.sh
+
+CONNECT_URL="https://saucelabs.com/downloads/sc-4.3-linux.tar.gz"
+CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
+CONNECT_DOWNLOAD="sc-4.3-linux.tar.gz"
+CONNECT_BIN="./sc-4.3-linux/bin/sc"
+READY_FILE="connect-ready-$RANDOM"
+
+# Get connect and start it
+mkdir -p $CONNECT_DIR
+cd $CONNECT_DIR
+curl $CONNECT_URL > $CONNECT_DOWNLOAD
+tar zxvf $CONNECT_DOWNLOAD
+rm $CONNECT_DOWNLOAD
+bash $CONNECT_BIN --readyfile $READY_FILE \
+  --tunnel-identifier $TRAVIS_JOB_NUMBER \
+  -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY &
+
+# Wait for Connect to be ready before exiting
+while [ ! -f $READY_FILE ]; do
+  sleep .5
+done
