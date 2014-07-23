a88b301f82e58dd6002aa16b05ced0ff5f453929
Andrew Harris <andrew@noblesamurai.com>
2014-07-23 18:34:05 +1000

    use localhost sauce connector

--- protractor.config.js
+++ protractor.config.js
@@ -11,9 +11,10 @@ var config = {
 }
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
-  config.sauceUser = process.env.SAUCE_USERNAME,
-  config.sauceKey = process.env.SAUCE_ACCESS_KEY,
+  config.seleniumAddress = 'http://localhost:4445/wd/hub';
   config.capabilities = {
+    'username': process.env.SAUCE_USERNAME,
+    'accessKey': process.env.SAUCE_ACCESS_KEY,
     'browserName': 'chrome',
     'platform': 'OS X 10.9',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
